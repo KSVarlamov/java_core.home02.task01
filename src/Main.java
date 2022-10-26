466a905a19fdd03da9b6f9ee18a51f6bd58397b0
KSVarlamov <varlamov.ks@mail.ru>
2022-10-26 12:56:21 +0300

    Решение задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+//                Отфильтруйте положительные числа.
+//                Найдите среди этих положительных чисел четные.
+//                Отсортируйте отфильтрованные числа в порядке возрастания.
+//                Выведите результат на экран.
+        List<Integer> result = new ArrayList<>();
+        for (int i : intList) {
+            if ((i > 0) && (i % 2 == 0)) {
+                result.add(i);
+            }
+        }
+        Collections.sort(result);
+        System.out.println(result);
     }
 }
